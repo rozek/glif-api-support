a4648e77244d9e7de8f1c04f1a1c8707f86ec9f5
Andreas Rozek <A.Rozek@gmx.de>
2025-02-20 15:25:17 +0100

    initial build

---
+++ dist/GlifInterfaces.d.ts
@@ -0,0 +1,15 @@
+/*******************************************************************************
+*                                                                              *
+*                                GlifInterfaces                                *
+*                                                                              *
+*******************************************************************************/
+/**** fencable - escapes "»" and "«" ****/
+export declare function fencable(Text: string): string;
+/**** fenced - "fences" a text between "»»»" and "«««" ****/
+export declare function fenced(Text: string): string;
+/**** unfenced - reverts text fencing ****/
+export declare function unfenced(Text: string): string;
+/**** LanguageOfText - detects the language a given text is written in ****/
+export declare const LanguageCodes: string[];
+export type LanguageCode = typeof LanguageCodes[number];
+export declare function LanguageOfText(Text: string): Promise<LanguageCode>;
