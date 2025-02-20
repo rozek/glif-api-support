0bcf045bb2a24b7a678ca3aff75af9e1e0b5b5aa
Andreas Rozek <A.Rozek@gmx.de>
2025-02-20 17:28:37 +0100

    Update GlifInterfaces.ts

--- src/GlifInterfaces.ts
+++ src/GlifInterfaces.ts
@@ -5,6 +5,8 @@
 *******************************************************************************/
 
   import {
+    throwError,
+    ValueIsOneOf,
     expectText,
   } from "javascript-interface-library"
 
@@ -47,7 +49,23 @@
   ]
   export type LanguageCode = typeof LanguageCodes[number]
 
-  const LanguageCodeSet = new Set(LanguageCodes)
+  export const Languages = [
+    'arabic', 'bengali', 'bulgarian', 'chinese', 'danish', 'german', 'english',
+    'estonian', 'finnish', 'french', 'greek', 'hebrew', 'hindi', 'italian',
+    'korean', 'croatian', 'dutch', 'portuguese', 'spanish', 'czech', 'hungarian'
+  ]
+  export type Language = typeof Languages[number]
+
+  const LanguageSet = Object.assign(Object.create(null),{
+    'ar': 'arabic',    'bn': 'bengali',    'bg': 'bulgarian',
+    'zh': 'chinese',   'da': 'danish',     'de': 'german',
+    'en': 'english',   'et': 'estonian',   'fi': 'finnish',
+    'fr': 'french',    'el': 'greek',      'iw': 'hebrew',
+    'hi': 'hindi',     'it': 'italian',    'ko': 'korean',
+    'hr': 'croatian',  'nl': 'dutch',      'pt': 'portuguese',
+    'es': 'spanish',   'cs': 'czech',      'hu': 'hungarian',
+    'unknown': 'unknown'
+  })
 
   export async function LanguageOfText (Text:string):Promise<LanguageCode> {
     expectText('text to be analyzed',Text)
@@ -57,7 +75,34 @@
       'cm7d23nop0000ona1b5b1cotg',[fencableText]
     ) as Indexable
     const LanguageCode = unfenced(Response.output as string).trim()
-    return (LanguageCodeSet.has(LanguageCode) ? LanguageCode : 'unknown')
+    return (LanguageCode in LanguageSet ? LanguageCode : 'unknown')
+  }
+
+/**** TranslationOfTextInto - translates a given text into a supported language ****/
+
+  export async function TranslationOfTextInto (
+    Text:string, TargetLanguage:Language|LanguageCode
+  ):Promise<string> {
+    expectText('text to be translated',Text)
+
+    switch (true) {
+      case (TargetLanguage ==  null):
+        throwError('MissingArgument: no "TargetLanguage" given')
+      case (TargetLanguage === 'unknown'):
+        return Text
+      case ValueIsOneOf(TargetLanguage,LanguageCodes):
+        TargetLanguage = LanguageSet[TargetLanguage]
+      case ValueIsOneOf(TargetLanguage,Languages):
+        break
+      default:
+        throwError('InvalidArgument:invalid "TargetLanguage" given')
+    }
+
+    const fencableText = fencable(Text)
+    const Response     = await GlifRunner.run(
+      'cm7dj2je40003yilbq4y0hl4h',[fencableText,TargetLanguage]
+    ) as Indexable
+    return unfenced(Response.output as string)
   }
 
 
